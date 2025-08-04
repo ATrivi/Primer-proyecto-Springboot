a8e75dc566cbb3dac59b0c324768c095799db22e
Alejandro <jandroth@hotmail.com>
2025-08-04 13:37:00 +0200

    implementacion_editar_y_eliminar

--- src/main/java/com/example/Primer_proyecto_Springboot/controller/PersonaController.java
+++ src/main/java/com/example/Primer_proyecto_Springboot/controller/PersonaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,7 +19,6 @@ import com.example.Primer_proyecto_Springboot.service.PersonaService;
 public class PersonaController {
 	
 	@Autowired
-	
 	private PersonaService personaService;
 	
 	
@@ -43,6 +43,26 @@ public class PersonaController {
 		
 		personaService.crearPersona(persona);
 		return "redirect:/personas";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String mostrarFormularioEditar(@PathVariable Long id, @ModelAttribute Persona persona, Model model) {
+	    model.addAttribute("persona", persona);
+	    model.addAttribute("accion", "/personas/editar/" + id);
+	    return "formulario";
+	}
+	
+	@PostMapping("/editar/{id}")
+	public String actualizarPersona (@PathVariable Long id, @ModelAttribute Persona persona) {
+		
+		personaService.actualizarPersona(id, persona);
+		return "redirect:/personas";
+	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eliminarPersona(@PathVariable Long id) {
+	    personaService.eliminarPersona(id);
+	    return "redirect:/personas";
 	}
 	}
 
